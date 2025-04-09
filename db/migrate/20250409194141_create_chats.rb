aa841caf50dacad1112c1321c9f1824568142b21
Rodrigo <rguzman1@miaundes.cl>
2025-04-09 16:08:16 -0400

    null false tablas 2 y 3

--- db/migrate/20250409194141_create_chats.rb
+++ db/migrate/20250409194141_create_chats.rb
@@ -1,10 +1,10 @@
 class CreateChats < ActiveRecord::Migration[8.0]
   def change
     create_table :chats do |t|
-      t.integer :sender_id
-      t.integer :receiver_id
+      t.integer :sender_id, null: false
+      t.integer :receiver_id, null: false
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
