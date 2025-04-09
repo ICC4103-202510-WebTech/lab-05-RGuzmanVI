e82af0abb380ca907d61c2fd24f35e224cf156ce
Rodrigo <rguzman1@miaundes.cl>
2025-04-09 16:14:26 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,29 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# Users
+10.times do |i|
+    User.create!(
+      email: "user#{i}@example.com",
+      first_name: "First#{i}",
+      last_name: "Last#{i}"
+    )
+  end
+  
+  # Chats
+  10.times do
+    Chat.create!(
+      sender_id: User.pluck(:id).sample,
+      receiver_id: User.pluck(:id).sample
+    )
+  end
+  
+  # Messages
+  10.times do
+    Message.create!(
+      chat_id: Chat.pluck(:id).sample,
+      user_id: User.pluck(:id).sample,
+      body: "Hello, this is a test message."
+    )
+  end
+  
