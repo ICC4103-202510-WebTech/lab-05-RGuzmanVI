fbe3838648d11d267610ea20ecf2cfa47d95c47d
Rodrigo <rguzman1@miaundes.cl>
2025-04-30 15:24:15 -0400

    columnas

--- db/migrate/20250430191021_add_chat_and_sender_to_messages.rb
+++ db/migrate/20250430191021_add_chat_and_sender_to_messages.rb
@@ -1,9 +1,9 @@
 class AddChatAndSenderToMessages < ActiveRecord::Migration[8.0]
   def change
-    add_column :messages, :chat_id, :bigint
-    add_column :messages, :sender_id, :bigint
+    add_column :messages, :chat_id, :bigint unless column_exists?(:messages, :chat_id)
+    add_column :messages, :user_id, :bigint unless column_exists?(:messages, :user_id)
 
-    add_foreign_key :messages, :chats
-    add_foreign_key :messages, :users, column: :sender_id
+    add_foreign_key :messages, :chats, column: :chat_id unless foreign_key_exists?(:messages, column: :chat_id)
+    add_foreign_key :messages, :users, column: :user_id unless foreign_key_exists?(:messages, column: :user_id)
   end
 end
