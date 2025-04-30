2522ff78ee4e9ecb1d91b094a3ce50aa4bb9bea5
Rodrigo <rguzman1@miaundes.cl>
2025-04-30 15:46:35 -0400

    asociaciones

---
+++ db/migrate/20250430191016_add_associations.rb
@@ -0,0 +1,10 @@
+class AddAssociations < ActiveRecord::Migration[8.0]
+  def change
+
+    add_foreign_key :chats, :users, column: :sender_id
+    add_foreign_key :chats, :users, column: :receiver_id
+
+    add_foreign_key :messages, :chats
+    add_foreign_key :messages, :users
+  end
+end
--- db/migrate/20250430191021_add_chat_and_sender_to_messages.rb
+++
@@ -1,9 +0,0 @@
-class AddChatAndSenderToMessages < ActiveRecord::Migration[8.0]
-  def change
-    add_column :messages, :chat_id, :bigint unless column_exists?(:messages, :chat_id)
-    add_column :messages, :user_id, :bigint unless column_exists?(:messages, :user_id)
-
-    add_foreign_key :messages, :chats, column: :chat_id unless foreign_key_exists?(:messages, column: :chat_id)
-    add_foreign_key :messages, :users, column: :user_id unless foreign_key_exists?(:messages, column: :user_id)
-  end
-end
