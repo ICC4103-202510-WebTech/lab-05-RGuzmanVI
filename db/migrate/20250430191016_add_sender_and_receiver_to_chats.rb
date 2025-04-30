49f15a0255769cb117722d254332be60bf3165ea
Rodrigo <rguzman1@miaundes.cl>
2025-04-30 15:35:48 -0400

    tabla2

--- db/migrate/20250430191016_add_sender_and_receiver_to_chats.rb
+++ db/migrate/20250430191016_add_sender_and_receiver_to_chats.rb
@@ -1,9 +1,9 @@
 class AddSenderAndReceiverToChats < ActiveRecord::Migration[8.0]
   def change
-    add_column :chats, :sender_id, :bigint
-    add_column :chats, :receiver_id, :bigint
+    add_column :chats, :sender_id, :bigint unless column_exists?(:chats, :sender_id)
+    add_column :chats, :receiver_id, :bigint unless column_exists?(:chats, :receiver_id)
 
-    add_foreign_key :chats, :users, column: :sender_id
-    add_foreign_key :chats, :users, column: :receiver_id
+    add_foreign_key :chats, :users, column: :sender_id unless foreign_key_exists?(:chats, column: :sender_id)
+    add_foreign_key :chats, :users, column: :receiver_id unless foreign_key_exists?(:chats, column: :receiver_id)
   end
 end
